5c787c699fedd0f385218816af084b0ce8a369c0
fernandominguez <dominguez.fernando@gmail.com>
2019-10-12 11:05:20 +0200

    Added DEFAULT_LOCALE value

--- nodejs-web-client/config/config.js
+++ nodejs-web-client/config/config.js
@@ -1,8 +1,9 @@
 module.exports = {
   API_HOST: "http://nodejs-server:5000",
+  DEFAULT_LOCALE: "es",
+  DOWNLOAD_DIR: "./tmp/download/",
   PORT: 80,
   SALT: "$2a$10$wENMOiXaNvkXN9BmCbh4ZO",
   SECRET_KEY: "R3g1Sg3Ns3cR3tK3y",
-  UPLOAD_DIR: "./tmp/upload/",
-  DOWNLOAD_DIR: "./tmp/download/"
+  UPLOAD_DIR: "./tmp/upload/"
 };
